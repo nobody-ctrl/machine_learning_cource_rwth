ea0ad5e4ee9cd2891b32dc1f3cccd8de265cf725
nobody-ctrl <anastasiia.petrova@protonmail.com>
2023-11-03 12:14:41 +0100

    Add task 6

---
+++ q6_expectation_maximization_python/regularize_cov.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def regularize_cov(covariance, epsilon):
+    # regularize a covariance matrix, by enforcing a minimum
+    # value on its singular values. Explanation see exercise sheet.
+    #
+    # INPUT:
+    #  covariance: matrix
+    #  epsilon:    minimum value for singular values
+    #
+    # OUTPUT:
+    # regularized_cov: reconstructed matrix
+
+    #####Insert your code here for subtask 6d#####
+    (D, X) = covariance.shape
+    regularized_cov = np.dot(epsilon, np.identity(D))+covariance 
+    return regularized_cov
